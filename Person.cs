de9dbc29a571072108068b4f60bac01e366a21e9
RemoteConnection <letuanmanh86@gmail.com>
2022-02-09 15:01:34 +0700

    InoChangedProperty

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DataBinding_wpf
+{
+    internal class Person : INotifyPropertyChanged
+    {
+        private string name;
+        public string PersonName { get { return name; } set { name = value; OnChangeProperty("PersonName"); } }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnChangeProperty(string info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
